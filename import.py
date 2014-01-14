801bd58bb063565313cea5ba1c9ffc54407f7d3d
Bryan Shelton <bryan@sheltonplace.com>
2014-01-14 13:43:16 -0800

    Add some geojson properties.

--- import.py
+++ import.py
@@ -4,6 +4,7 @@ from shapely.geometry import LineString, mapping, shape
 from shapely.ops import linemerge, unary_union
 shapes = {}
 trip_shapes = {}
+routes = {}
 
 # Parse shapes into a dict
 with open('gtfs/shapes.txt') as f:
@@ -22,13 +23,30 @@ with open('gtfs/trips.txt') as f:
       trip_shapes[short_name] = {}
     trip_shapes[short_name][shape_id] = shapes[shape_id]
 
+# Load some data about routes
+with open('gtfs/routes.txt') as f:
+  reader = csv.DictReader(f)
+  for row in reader:
+    routes[row['route_short_name']] = row
+
 # Write out merged Linestrings for each route
 for trip_name, shapes in trip_shapes.items():
+    route = routes[trip_name]
     lines = []
     for k,v in shapes.items():
       line = LineString(v)
       lines.append(line)
 
     merged_line = linemerge(lines)
+    
+    # Map to a Geojson like object
+    geojson = mapping(merged_line)
+    geojson['properties'] = {
+      'color': '#'+route['route_color'],
+      'name': route['route_short_name'],
+      'description': route['route_desc']
+    }
+
+    # Write to disk
     with open('route_shapes/'+trip_name+'.geojson', 'w+') as f:
-      f.write(json.dumps(mapping(merged_line)))
+      f.write(json.dumps(geojson))
